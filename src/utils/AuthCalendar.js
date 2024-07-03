2d54d8bc8a287c324c56f6e22e482e2cdf817795
Douglas Augusto <sunshinedn2003@gmail.com>
2024-07-03 17:20:43 -0300

    if

--- src/utils/AuthCalendar.js
+++ src/utils/AuthCalendar.js
@@ -2,13 +2,15 @@ const { google } = require('googleapis');
 
 class AuthCalendar {
   authenticate(account_id) {
-    const service_account_path = `../config/service_account_${account_id}.json`;
-    const serviceAccount = require(service_account_path);
-    return new google.auth.JWT({
-      email: serviceAccount.client_email,
-      key: serviceAccount.private_key,
-      scopes: ['https://www.googleapis.com/auth/calendar']
-    });
+    if (account_id) {
+      const service_account_path = `../config/service_account_${account_id}.json`;
+      const serviceAccount = require(service_account_path);
+      return new google.auth.JWT({
+        email: serviceAccount.client_email,
+        key: serviceAccount.private_key,
+        scopes: ['https://www.googleapis.com/auth/calendar']
+      });
+    }
   }
 }
 
