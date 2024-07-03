a3a54aafe41a11fa0fb5d21d5e32d1202da61f2d
Douglas Augusto <sunshinedn2003@gmail.com>
2024-07-03 17:27:51 -0300

    ajustes auth

--- src/utils/AuthCalendar.js
+++ src/utils/AuthCalendar.js
@@ -1,13 +1,19 @@
 const { google } = require('googleapis');
+const service_account_ai_atende = require('../config/service_account_31205035.json');
+const service_account_dental_sante = require('../config/service_account_32000011.json');
 
 class AuthCalendar {
   authenticate(account_id) {
-    if (account_id) {
-      const service_account_path = `../config/service_account_${account_id}.json`;
-      const serviceAccount = require(service_account_path);
+    if (account_id === 32000011) {
       return new google.auth.JWT({
-        email: serviceAccount.client_email,
-        key: serviceAccount.private_key,
+        email: service_account_dental_sante.client_email,
+        key: service_account_dental_sante.private_key,
+        scopes: ['https://www.googleapis.com/auth/calendar']
+      });
+    }else if (account_id === 31205035){
+      return new google.auth.JWT({
+        email: service_account_ai_atende.client_email,
+        key: service_account_ai_atende.private_key,
         scopes: ['https://www.googleapis.com/auth/calendar']
       });
     }
