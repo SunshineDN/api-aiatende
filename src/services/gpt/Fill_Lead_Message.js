a524dae0b98d6635b2eb06f141411dea6fd7cf72
Douglas Augusto <sunshinedn2003@gmail.com>
2024-07-30 17:11:03 -0300

    message sorted reverse

--- src/services/gpt/Fill_Lead_Message.js
+++ src/services/gpt/Fill_Lead_Message.js
@@ -20,7 +20,8 @@ const Fill_Lead_Message = async (payload, access_token = null) => {
 
     if (leadMessage_isFilled?.values[0]?.value) {
       const messageSplit = leadMessage_isFilled?.values[0]?.value.split('\n');
-      message = `${messageSplit.filter((_, index) => index < 2).join('\n')}
+      const sortedMessage = messageSplit?.reverse();
+      message = `${sortedMessage.filter((_, index) => index < 2).join('\n')}
 ${payload.text_audio.replace(/\+/g, ' ')}`;
     } else {
       message = payload.text_audio.replace(/\+/g, ' ');
