ba1831115bab0a6b5d7783b0f3fc782ea614c749
Douglas Augusto <sunshinedn2003@gmail.com>
2024-08-24 16:47:22 -0300

    descida text to speech

--- src/gpt-req/Aquecimento.js
+++ src/gpt-req/Aquecimento.js
@@ -23,9 +23,9 @@ class Aquecimento {
       access_token = process.env.ACCESS_TOKEN || await GetAccessToken(req.body);
       console.log('Mensagem enviada para o assistente:', data.text);
       const { message } = await OpenAIController.generateMessage(data);
-      await TextToSpeech(req.body, access_token);
       console.log('Resposta recebida do assistente:', message);
       await SendMessage(req.body, message, access_token);
+      await TextToSpeech(req.body, access_token);
       res.status(200).send({ message: 'Mensagem enviada com sucesso para o assistente', response: message });
     } catch (error) {
       console.log(`Erro ao enviar mensagem para o assistente: ${error.message}`);
