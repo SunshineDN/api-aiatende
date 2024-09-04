2d656e9df49280c426eadfa0022e1db8afaed9b7
Douglas Augusto <sunshinedn2003@gmail.com>
2024-09-04 13:42:23 -0300

    ok

--- src/services/kommo/GetMessageReceived.js
+++ src/services/kommo/GetMessageReceived.js
@@ -9,7 +9,7 @@ const GetMessageReceived = async (payload, access_token = null) => {
     }
     const user = await GetUser(payload, false, access_token);
     const message_received = user?.custom_fields_values?.filter(field => field.field_name === 'GPT | Message received')[0];
-    return message_received?.values[0]?.value;
+    return message_received?.values[0]?.value || 'ok';
   } catch (error) {
     throw new Error(`Erro ao buscar mensagem recebida: ${error.message}`);
   }
