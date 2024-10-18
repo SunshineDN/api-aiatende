73e2ccbf04acc2f86f1d6fcbc3ff5e54dc63c0d8
Douglas Augusto <sunshinedn2003@gmail.com>
2024-10-18 13:32:44 -0300

    upd lead query params

--- teste.js
+++ teste.js
@@ -145,25 +145,43 @@
 
 // console.log(message);
 
-const fatherFieldValues = 'Nome: Teste1; Bairro: teste2; Dentista: teste4; Data do Agendamento: teste5; Telefone: null';
+// const fatherFieldValues = 'Nome: Teste1; Bairro: teste2; Dentista: teste4; Data do Agendamento: teste5; Telefone: null';
 
-// Separar cada valor do pai em uma variavel especifica sem o nome do campo
-const nome = fatherFieldValues.split('; ').filter(value => value.includes('Nome:'))[0]?.split(': ')[1] || 'null';
-const bairro = fatherFieldValues.split('; ').filter(value => value.includes('Bairro:'))[0]?.split(': ')[1] || 'null';
-const data_nascimento = fatherFieldValues.split('; ').filter(value => value.includes('Data de Nascimento:'))[0]?.split(': ')[1] || 'null';
-const dentista = fatherFieldValues.split('; ').filter(value => value.includes('Dentista:'))[0]?.split(': ')[1] || 'null';
-const data_agendamento = fatherFieldValues.split('; ').filter(value => value.includes('Data do Agendamento:'))[0]?.split(': ')[1] || 'null';
-const telefone = fatherFieldValues.split('; ').filter(value => value.includes('Telefone:'))[0]?.split(': ')[1] || 'null';
+// // Separar cada valor do pai em uma variavel especifica sem o nome do campo
+// const nome = fatherFieldValues.split('; ').filter(value => value.includes('Nome:'))[0]?.split(': ')[1] || 'null';
+// const bairro = fatherFieldValues.split('; ').filter(value => value.includes('Bairro:'))[0]?.split(': ')[1] || 'null';
+// const data_nascimento = fatherFieldValues.split('; ').filter(value => value.includes('Data de Nascimento:'))[0]?.split(': ')[1] || 'null';
+// const dentista = fatherFieldValues.split('; ').filter(value => value.includes('Dentista:'))[0]?.split(': ')[1] || 'null';
+// const data_agendamento = fatherFieldValues.split('; ').filter(value => value.includes('Data do Agendamento:'))[0]?.split(': ')[1] || 'null';
+// const telefone = fatherFieldValues.split('; ').filter(value => value.includes('Telefone:'))[0]?.split(': ')[1] || 'null';
 
-// Montar um objeto apenas com os dados existentes que forem diferentes de 'null'
-const data = {
-  nome: nome !== 'null' ? nome : null,
-  bairro: bairro !== 'null' ? bairro : null,
-  data_nascimento: data_nascimento !== 'null' ? data_nascimento : null,
-  dentista: dentista !== 'null' ? dentista : null,
-  data_agendamento: data_agendamento !== 'null' ? data_agendamento : null,
-  telefone: telefone !== 'null' ? telefone : null
+// // Montar um objeto apenas com os dados existentes que forem diferentes de 'null'
+// const data = {
+//   nome: nome !== 'null' ? nome : null,
+//   bairro: bairro !== 'null' ? bairro : null,
+//   data_nascimento: data_nascimento !== 'null' ? data_nascimento : null,
+//   dentista: dentista !== 'null' ? dentista : null,
+//   data_agendamento: data_agendamento !== 'null' ? data_agendamento : null,
+//   telefone: telefone !== 'null' ? telefone : null
+// };
+// Object?.keys(data)?.forEach(key => data[key] === null && delete data[key]);
+
+// console.log(data);
+
+const obj = {
+  nome: 'Douglas Augusto Cabral da Silva',
+  bairro: 'Candeias',
+  birthdate: '11/03/2003',
+  dentist: 'Dra. Juliana Leite',
+  schedule_date: 'null',
+  reason: 'Avaliação Geral',
+  phone: '+558196724310'
 };
-Object?.keys(data)?.forEach(key => data[key] === null && delete data[key]);
 
-console.log(data);
+const { nome, bairro, birthdate, dentist, schedule_date, reason, phone } = obj;
+
+if (!nome || !schedule_date || !phone) {
+  console.log('Erro ao criar / atualizar lead via agendamento por VOZ: Dados obrigatórios não informados');
+} else {
+  console.log('Lead criado via agendamento por VOZ com sucesso!');
+}
