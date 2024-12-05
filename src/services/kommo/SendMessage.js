3ffaf6d41f25816507e45b47839f4609a72e4c4d
Douglas Augusto <sunshinedn2003@gmail.com>
2024-12-05 11:09:34 -0300

    audio removed from sendmessage

--- src/services/kommo/SendMessage.js
+++ src/services/kommo/SendMessage.js
@@ -1,14 +1,14 @@
 // const TextToSpeech = require('../gpt/TextToSpeech');
 const GetCustomFields = require('./GetCustomFields');
-const GetUser = require('./GetUser');
+// const GetUser = require('./GetUser');
 const UpdateLead = require('./UpdateLead');
 
 const SendMessage = async (body, audio, message, access_token) => {
   try {
-    const user = await GetUser(body, false, access_token);
+    // const user = await GetUser(body, false, access_token);
     
-    const canal_field = user?.custom_fields_values?.filter(field => field.field_name === 'CANAL DE ENTRADA')[0];
-    const canal = canal_field?.values[0]?.value;
+    // const canal_field = user?.custom_fields_values?.filter(field => field.field_name === 'CANAL DE ENTRADA')[0];
+    // const canal = canal_field?.values[0]?.value;
 
     const custom_fields = await GetCustomFields(body, access_token);
     const answer = custom_fields?.filter(field => field.name === 'GPT | Answer')[0];
@@ -33,10 +33,10 @@ const SendMessage = async (body, audio, message, access_token) => {
         }
       ]
     };
-    if (audio && canal !== '02 - WHATSAPP API' && canal !== '03 - REDE SOCIAL') {
-      console.log('Lead precisa de áudio...');
-      // await TextToSpeech(body, message, access_token);
-    }
+    // if (audio && canal !== '02 - WHATSAPP API' && canal !== '03 - REDE SOCIAL') {
+    //   console.log('Lead precisa de áudio...');
+    //   await TextToSpeech(body, message, access_token);
+    // }
     await UpdateLead(body, data, access_token);
     return;
   } catch (e) {
