5685dd9af81140377383e4ebb4698609b0c3ba34
Douglas Augusto <sunshinedn2003@gmail.com>
2024-12-09 16:06:25 -0300

    date function

---
+++ src/utils/BrazilianDate.js
@@ -0,0 +1,45 @@
+class BrazilianDate {
+  static getLocalDate() {
+    const date = new Date();
+    const options = {
+      timeZone: 'America/Recife',
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric'
+    };
+    const localDate = date.toLocaleString('pt-BR', options);
+
+    return localDate;
+  }
+
+  static getLocalTime() {
+    const date = new Date();
+    const options = {
+      timeZone: 'America/Recife',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    };
+    const localTime = date.toLocaleString('pt-BR', options);
+
+    return localTime;
+  }
+
+  static getLocalDateTime() {
+    return `${BrazilianDate.getLocalDate()} ${BrazilianDate.getLocalTime()}`;
+  }
+
+  static getLocalWeekDay() {
+    const date = new Date();
+    const options = {
+      timeZone: 'America/Recife',
+      weekday: 'long'
+    };
+    const weekDay = date.toLocaleDateString('pt-BR', options);
+    const weekDayFormatted = weekDay.charAt(0).toUpperCase() + weekDay.slice(1);
+
+    return weekDayFormatted;
+  }
+}
+
+module.exports = BrazilianDate;
