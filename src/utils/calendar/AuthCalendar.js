77aaecc77dd0b46b01ecfd3f2842224add0e0819
Douglas Augusto <sunshinedn2003@gmail.com>
2025-01-06 09:23:20 -0300

    service account

--- src/utils/calendar/AuthCalendar.js
+++ src/utils/calendar/AuthCalendar.js
@@ -1,12 +1,12 @@
 
 import { google } from 'googleapis';
-import service_account_dental_sante from '../../config/service_account_32000011.json' with { type: 'json' };
+import service_account from '../../config/service_account.json' with { type: 'json' };
 
 export class AuthCalendar {
   static authenticate() {
     return new google.auth.JWT({
-      email: service_account_dental_sante.client_email,
-      key: service_account_dental_sante.private_key,
+      email: service_account.client_email,
+      key: service_account.private_key,
       scopes: ['https://www.googleapis.com/auth/calendar']
     });
   }
