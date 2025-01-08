c4ddb7955448e3bd90b325cbb1d1d3722dbf68e2
Douglas Augusto <sunshinedn2003@gmail.com>
2025-01-08 16:29:14 -0300

    routes upd adriano

--- src/routes/gpt/v2/index.js
+++ src/routes/gpt/v2/index.js
@@ -13,23 +13,11 @@ import cutucadaPromptRouter from './cutucada/prompt/index.js';
 router.use('/cutucada/assistant', cutucadaAssistenteRouter);
 router.use('/cutucada/prompt', cutucadaPromptRouter);
 
-// Roteamento para as subpastas de /gpt/v2/dados
-import dadosAssistenteRouter from './dados/assistente/index.js';
-import dadosPromptRouter from './dados/prompt/index.js';
-router.use('/dados/assistant', dadosAssistenteRouter);
-router.use('/dados/prompt', dadosPromptRouter);
-
 // Roteamento para as subpastas de /gpt/v2/global
 import globalAssistenteRouter from './global/assistente/index.js';
 import globalPromptRouter from './global/prompt/index.js';
 router.use('/global/assistant', globalAssistenteRouter);
 router.use('/global/prompt', globalPromptRouter);
-
-// Roteamento para as subpastas de /gpt/v2/preagendamento
-import preagendamentoAssistenteRouter from './preagendamento/assistente/index.js';
-import preagendamentoPromptRouter from './preagendamento/prompt/index.js';
-router.use('/preagendamento/assistant', preagendamentoAssistenteRouter);
-router.use('/preagendamento/prompt', preagendamentoPromptRouter);
 
 // Roteamento para as subpastas de /gpt/v2/qualificado
 import qualificadoAssistenteRouter from './qualificado/assistente/index.js';
