ac122019f4b96feac2d2d3f83653b7c07479b40a
Douglas Augusto <sunshinedn2003@gmail.com>
2025-01-09 01:11:43 -0300

    locale

--- src/utils/DateUtils.js
+++ src/utils/DateUtils.js
@@ -1,6 +1,9 @@
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat.js';
 import styled from './log/styledLog.js';
+import 'dayjs/locale/pt-br.js';
+
+dayjs.locale('pt-br');
 
 export default class DateUtils {
   static convertDateToMs(dateString) {
