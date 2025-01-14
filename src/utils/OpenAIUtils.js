9da061d1d0e6024ec4cf82fa6a82bb7e85cdc3e4
Douglas Augusto <sunshinedn2003@gmail.com>
2025-01-13 22:34:31 -0300

    this openai

--- src/utils/OpenAIUtils.js
+++ src/utils/OpenAIUtils.js
@@ -50,7 +50,7 @@ export default class OpenAIUtils {
 
     const file = fs.createReadStream(filePath);
 
-    const transcription = await openai.audio.transcriptions.create({
+    const transcription = await this.openai.audio.transcriptions.create({
       file: file,
       model: 'whisper-1',
     });
