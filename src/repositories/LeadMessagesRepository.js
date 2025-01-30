19f320fe7a4ab5a08d08877cc6de0021dcdb615b
Douglas Augusto <sunshinedn2003@gmail.com>
2025-01-30 18:28:23 -0300

    last and recent messages

--- src/repositories/LeadMessagesRepository.js
+++ src/repositories/LeadMessagesRepository.js
@@ -38,50 +38,32 @@ export default class LeadMessagesRepository extends BaseRepository {
 
   async getLastMessages(lead_id, limit = 3) {
     const lead_message = await this.findOne({ where: { id: Number(lead_id) } });
-    if (!lead_message) return '';
+    if (!lead_message?.messages?.length) return null;
 
-    const lead_messages = lead_message.messages;
-    if (!lead_messages || !lead_messages.length) return '';
+    const messages = lead_message.messages;
 
-    const messages = lead_messages.slice(-limit).map(msg => msg.lead_message);
-    return messages.join('\n');
+    return messages.slice(-limit).map(msg => msg.lead_message).join('\n');
   }
 
   async getRecentMessages(lead_id) {
     const lead_message = await this.findOne({ where: { id: Number(lead_id) } });
-    if (!lead_message) return '';
+    if (!lead_message?.messages?.length) return null;
 
-    const lead_messages = lead_message.messages;
-    if (!lead_messages || !lead_messages.length) return '';
+    const last_timestamp = await new LeadThreadRepository().getLastTimestamp(Number(lead_id));
+    last_timestamp?.setMilliseconds(last_timestamp.getMilliseconds() - 1000);
 
-    const last_timestamp = await new LeadThreadRepository().getLastTimestamp(lead_id);
+    const messages = lead_message.messages;
+    const recent_messages = [];
 
-    let messages = lead_messages;
-    if (last_timestamp) {
-      messages = lead_messages.filter(msg => new Date(Number(msg.created_at) * 1000) > last_timestamp);
+    for (const msg of messages) {
+      const created_at = new Date(Number(msg.created_at) * 1000);
+      if (created_at > last_timestamp) {
+        recent_messages.push(msg.lead_message);
+      }
     }
 
-    if (!messages.length) return '';
-
-    return messages.map(msg => msg.lead_message).join('\n');
+    return recent_messages.join('\n') || null
   }
-
-  // async getLastAndRecentMessages(lead_id) {
-  //   const lead_message = await this.findOne({ where: { id: Number(lead_id) } });
-  //   if (!lead_message) return null;
-
-  //   const lead_messages = lead_message.messages;
-  //   if (!lead_messages || !lead_messages.length) return null;
-
-  //   const last_timestamp = await new LeadThreadRepository().getLastTimestamp(Number(lead_id));
-  //   const messages = lead_messages.filter(msg => new Date(Number(msg.created_at) * 1000) > last_timestamp);
-  //   if (!messages.length) return null;
-
-  //   return {
-  //     last_messages: lead_messages.slice(-3).map(msg => msg.lead_message).join('\n'),
-  //     recent_messages: messages.map(msg => msg.lead_message).join('\n')
-  //   };
-  // }
 
   async getLastAndRecentMessages(lead_id) {
     const lead_message = await this.findOne({ where: { id: Number(lead_id) } });
@@ -105,4 +87,51 @@ export default class LeadMessagesRepository extends BaseRepository {
       recent_messages: recent_messages.join('\n') || null
     };
   }
+
+  // async getLastMessages(lead_id, limit = 3) {
+  //   const lead_message = await this.findOne({ where: { id: Number(lead_id) } });
+  //   if (!lead_message) return '';
+
+  //   const lead_messages = lead_message.messages;
+  //   if (!lead_messages || !lead_messages.length) return '';
+
+  //   const messages = lead_messages.slice(-limit).map(msg => msg.lead_message);
+  //   return messages.join('\n');
+  // }
+
+  // async getRecentMessages(lead_id) {
+  //   const lead_message = await this.findOne({ where: { id: Number(lead_id) } });
+  //   if (!lead_message) return '';
+
+  //   const lead_messages = lead_message.messages;
+  //   if (!lead_messages || !lead_messages.length) return '';
+
+  //   const last_timestamp = await new LeadThreadRepository().getLastTimestamp(lead_id);
+
+  //   let messages = lead_messages;
+  //   if (last_timestamp) {
+  //     messages = lead_messages.filter(msg => new Date(Number(msg.created_at) * 1000) > last_timestamp);
+  //   }
+
+  //   if (!messages.length) return '';
+
+  //   return messages.map(msg => msg.lead_message).join('\n');
+  // }
+
+  // async getLastAndRecentMessages(lead_id) {
+  //   const lead_message = await this.findOne({ where: { id: Number(lead_id) } });
+  //   if (!lead_message) return null;
+
+  //   const lead_messages = lead_message.messages;
+  //   if (!lead_messages || !lead_messages.length) return null;
+
+  //   const last_timestamp = await new LeadThreadRepository().getLastTimestamp(Number(lead_id));
+  //   const messages = lead_messages.filter(msg => new Date(Number(msg.created_at) * 1000) > last_timestamp);
+  //   if (!messages.length) return null;
+
+  //   return {
+  //     last_messages: lead_messages.slice(-3).map(msg => msg.lead_message).join('\n'),
+  //     recent_messages: messages.map(msg => msg.lead_message).join('\n')
+  //   };
+  // }
 }
