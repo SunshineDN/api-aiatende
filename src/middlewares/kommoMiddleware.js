36988b2c1528995267fec9c062490dbba76b4756
Douglas Augusto <sunshinedn2003@gmail.com>
2025-02-11 16:51:53 -0300

    schedule lead

--- src/middlewares/kommoMiddleware.js
+++ src/middlewares/kommoMiddleware.js
@@ -18,7 +18,7 @@ export default function kommoMiddleware(req, _, next) {
   const account_domain = `https://${account_subdomain}.kommo.com`;
 
   req.body = {
-    lead_id,
+    lead_id: Number(lead_id),
     old_status_id,
     old_pipeline_id,
     status_id,
