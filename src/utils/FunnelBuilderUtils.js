e39bac08182158b28c699bac06c72e97388ad9da
Douglas Augusto <sunshinedn2003@gmail.com>
2025-02-19 14:29:32 -0300

    asasdkqjw djkha sjbdcaskjjbdaskjdn

--- src/utils/FunnelBuilderUtils.js
+++ src/utils/FunnelBuilderUtils.js
@@ -1,5 +1,10 @@
 export default class FunnelBuilderUtils {
   static identifyAnswer(obj) {
+    if (obj?.metadata?.length === 0) {
+      return { type: 'not_found', value: null };
+    }
+
+    const metadata = obj.metadata;
     
   }
-}
+};
