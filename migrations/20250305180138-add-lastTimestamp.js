0a7cb73802d41e72bdd82f9262a10875bc33042d
Douglas Augusto <sunshinedn2003@gmail.com>
2025-03-05 15:44:27 -0300

    migrations

---
+++ migrations/20250305180138-add-lastTimestamp.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.addColumn(
+          'lead_threads',
+          'assistant_messages',
+          {
+            type: DataTypes.ARRAY(DataTypes.ARRAY(DataTypes.JSON)),
+            after: "assistant_id"
+          },
+          { transaction: t },
+        ),
+        queryInterface.addColumn(
+          'lead_threads',
+          'lastTimestamp',
+          {
+            type: Sequelize.DataTypes.DATE,
+            after: "assistant_messages"
+          },
+          { transaction: t },
+        ),
+      ]);
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+  }
+};
