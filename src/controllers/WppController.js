5ded1ca8c94dc1ff4a68df97249442dfdafc78f8
Douglas Augusto <sunshinedn2003@gmail.com>
2025-04-17 13:41:02 -0300

    migrations and wpp services upd

--- src/controllers/WppController.js
+++ src/controllers/WppController.js
@@ -11,14 +11,15 @@ export default class WppController {
     * @param { import('express').Request } req
     * @param { import('express').Response } res
     */
-  async handleWabhookReceived(req, res) {
+  async handleWebhookReceived(req, res) {
     try {
+      const whatsAppNumber = process.env.WHATSAPP_NUMBER; // Número do WhatsApp no padrao 558112345678
       const { query } = req;
       styled.infodir(query);
       const hash = StaticUtils.generateSimpleHash();
-      const text = await this.wppServices.handleWabhookReceived(query, hash);
+      const text = await this.wppServices.handleWebhookReceived(query, hash);
       styled.success('Webhook received and handled');
-      res.redirect(`https://wa.me/558130930133?text=[ ${hash} -> *NÂO APAGUE ESSA MENSAGEM* ]\n${text}`);
+      res.redirect(`https://wa.me/${whatsAppNumber}?text=[ ${hash} ]\n${text}`);
     } catch (error) {
       styled.error('Error in webhook', error);
       res.status(500).send('Internal Server Error');
