e3c43a8ca8ff9af7fd6c275dbe20f28c3faae6e9
Douglas Augusto <sunshinedn2003@gmail.com>
2025-06-13 14:54:09 -0300

    calendar id

--- src/config/calendarId.js
+++ src/config/calendarId.js
@@ -1,6 +1,3 @@
 export const CalendarId = {
-  juliana: 'ff3376c2855b6c18044adc7017c77b2f41177f1705a27f850339f91568c468fb@group.calendar.google.com',
-  odontopediatria: 'b03e14eeb7ca37072b55f11a8e39c403f52ecfbbb398cb9b6c6c2314d92839b1@group.calendar.google.com',
-  dentistas: 'clinicadentalsante@gmail.com',
-  closed: '22f9847c598038ad46a72f2a00c1eac7cd54c481208a6c7cb138cc18f704b37a@group.calendar.google.com'
+  nelsoncoutinho: 'nelsonaiatende@gmail.com',
 };
